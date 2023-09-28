198ddcba266c30ce3a552da470d9c485c3af7806
Rosalia Dina Marina S <rosalia03rrrbkl@gmail.com>
2023-09-28 16:52:48 +0700

    Mini Project Revou

---
+++ js/script.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('button');
+    button.addEventListener('click', function(event) {
+        var nameField = document.getElementsByName('your-name')[0];
+        var emailField = document.getElementsByName('email')[0];
+        var interestField = document.getElementsByName('interest')[0];
+        
+        var name = nameField.value;
+        var email = emailField.value;
+        var interest = interestField.value;
+
+        var isValid = true;
+
+        if (name === "") {
+            alert("Nama wajib diisi!");
+            event.preventDefault();
+            isValid = false;
+            nameField.style.border = '2px solid red';
+        } else {
+            nameField.style.border = '2px solid blue';
+        }
+
+        if (email === "") {
+            alert("Email wajib diisi!");
+            event.preventDefault();
+            isValid = false;
+            emailField.style.border = '2px solid red';
+        } else if (!validateEmail(email)) {
+            alert("Format email tidak valid!");
+            event.preventDefault();
+            isValid = false;
+            emailField.style.border = '2px solid red';
+        } else {
+            emailField.style.border = '2px solid blue';
+        }
+
+        if (interest === "") {
+            alert("Pilihan minat wajib dipilih!");
+            event.preventDefault();
+            isValid = false;
+            interestField.style.border = '2px solid red';
+        } else {
+            interestField.style.border = '2px solid blue';
+        }
+
+        if (isValid) {
+            alert("Formulir berhasil dikirim!");
+            document.getElementById('top-up').style.border = '2px solid blue';
+        }
+    });
+
+    function validateEmail(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+});
