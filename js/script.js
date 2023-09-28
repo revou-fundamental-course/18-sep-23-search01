c52bacc7d6cf482df84aaa6cfc05b628e97bf1ad
Rosalia Dina Marina S <rosalia03rrrbkl@gmail.com>
2023-09-28 18:19:09 +0700

    Mini Project RevoU

--- js/script.js
+++ js/script.js
@@ -54,3 +54,25 @@ document.addEventListener('DOMContentLoaded', function() {
         return re.test(email);
     }
 });
+
+
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 10000);
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    showSlides();
+});
